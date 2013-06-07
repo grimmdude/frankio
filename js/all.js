adb3762e0c0739d4884b21056894dffbc26cba70
grimmdude <grimmdude@gmail.com>
2013-06-06 23:09:35 -0400

    js work

--- js/all.js
+++ js/all.js
@@ -1,98 +1,102 @@
 $(function() {
-	// Enable input
-	$('input').prop('disabled',false);
-	
 	// Array to save user inputs
 	var user_input = [];
 	var user_input_pointer;
 	
-	// Use up/down keys to cycle through past questions
-	$('input').keydown(function(e) {
-		// Up
-		if (e.keyCode == 38) {
-			if (user_input_pointer > 0) {
-				user_input_pointer--;
-				$(this).val(user_input[user_input_pointer]);
+	// Enable input
+	$('input').prop('disabled',false)
+		// Use up/down keys to cycle through past questions
+		.keydown(function(e) {
+			// Up
+			if (e.keyCode == 38) {
+				if (user_input_pointer > 0) {
+					user_input_pointer--;
+					$(this).val(user_input[user_input_pointer]);
+				}
+				//console.log(user_input_pointer);
+				return false;
 			}
-			//console.log(user_input_pointer);
-			return false;
-		}
-		// Down
-		if (e.keyCode == 40) {
-			if (user_input_pointer <= user_input.length-1 && user_input_pointer >= 0) {
-				user_input_pointer++;
-				$(this).val(user_input[user_input_pointer]);
+			// Down
+			if (e.keyCode == 40) {
+				if (user_input_pointer <= user_input.length-1 && user_input_pointer >= 0) {
+					user_input_pointer++;
+					$(this).val(user_input[user_input_pointer]);
+				}
+				//console.log(user_input_pointer);
+				return false;
 			}
-			//console.log(user_input_pointer);
-			return false;
-		}
-	});
-	
-	
-	$('input').ajaxStart(function() {
-		$(this).prop('disabled',true);
-	});
-	$('input').ajaxStop(function() {
-		$(this).prop('disabled',false);
-	});
-	$('input').change(function() {
-		var input = jQuery(this).val();
+		})
+		.ajaxStart(function() {
+			$(this).prop('disabled',true);
+		})
+		.ajaxStop(function() {
+			$(this).prop('disabled',false);
+		})
+		.change(function() {
+			var input = jQuery(this).val();
 		
-		// If this is a calculation input use JS
-		var calc_pattern = new RegExp("^([0-9\(\)]+[\-\/\+\*]?)+[0-9\(\)]+$");
-		var number_pattern = new RegExp("^[0-9\(\)]+[\-\/\+\*]?$")
-		if (number_pattern.test(input)) {
-			$('#response').text('I can do calculations, try 4 + 4');
-		}
-		else if (calc_pattern.test(input)) {
-			$('#response').text(input + '=' + eval(input));
-		}
-		else {
-			$.ajax({
-			  	url: 'index.php',
-				type: 'POST',
-				dataType: "json",
-				data: { 
-					action: "true",
-					input: input,
-					salt: jQuery('#salt').val()
-					},
-			  		success: function(data) {
-						if (Object.prototype.toString.call( data.output ) === '[object Array]') {
-							$('#response').html('<ul></ul>');
-							for (var i = 0; i < data.output.length; i++) {
-								$('#response ul').append('<li>' + data.output[i] + '</li>');
+			// If this is a calculation input use JS
+			var calc_pattern = new RegExp("^([0-9\(\)]+[\-\/\+\*]?)+[0-9\(\)]+$");
+			var number_pattern = new RegExp("^[0-9\(\)]+[\-\/\+\*]?$")
+			if (number_pattern.test(input)) {
+				$('#response').text('I can do calculations, try 4 + 4');
+			}
+			else if (calc_pattern.test(input)) {
+				$('#response').text(input + '=' + eval(input));
+			}
+			else {
+				$.ajax({
+				  	url: 'index.php',
+					type: 'POST',
+					dataType: "json",
+					data: { 
+						action: "true",
+						input: input,
+						salt: jQuery('#salt').val()
+						},
+				  		success: function(data) {
+							console.log(data);
+							if ($.isArray(data.output)) {
+								// If this is an object then display in a table.
+								if ($.isPlainObject(data.output[0])) {
+									for (var i=0; i < data.output.length; i++) {
+										for (key in data.output[i]) {
+											console.log(data.output[i][key]);
+										}
+									}
+								}
+								// If this is an array display in a list.
+								else {
+									$('#response').html('<ul></ul>');
+									for (var i = 0; i < data.output.length; i++) {
+										$('#response ul').append('<li>' + data.output[i] + '</li>');
+									}	
+								}
 							}
-						}
-						else {
-						$('#response').html(data.output);	
-						}
+							else {
+								$('#response').html(data.output);	
+							}
 						
-						$('#response a').attr('target','_blank');
-						if (data.type == 'wiki') {
-							$('#response').css('font-size','14px');
-						}
-						else {
-							$('#response').css('font-size','');
-						}
-						
-						// Show options if available
-						if (data.options) {
-							$('#options').empty();
-							$.each(data.options, function(index, value) {
-								$('#options').append('<a href="javascript:;"><span class="label label-info" style="margin-right:7px;">'+value+'<span></a>');
-							});
+							// Show options if available
+							if (data.options) {
+								$('#options').empty();
+								$.each(data.options, function(index, value) {
+									$('#options').append('<a href="javascript:;"><span class="label label-info" style="margin-right:7px;">'+value+'<span></a>');
+								});
 							
-							$('#options a').click(function() {
-								jQuery('#input').val(jQuery(this).find('span').text()).change();
-							});
-						}
+								$('#options a').click(function() {
+									$('#input').val(jQuery(this).find('span').text()).change();
+								});
+							}
 
-						user_input.push(data.question);
-						user_input_pointer = user_input.length - 1;
-				  	}
-			});	
-		}
-		$(this).val('');
-	});	
+							user_input.push(data.question);
+							user_input_pointer = user_input.length - 1;
+					  	},
+						error: function(e) {
+							$('#response').text("Couldn't talk to Frank IO.");
+						}
+				});	
+			}
+			$(this).val('');
+		});	
 });
